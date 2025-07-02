06c61480f1a45088ea6e5ef370c6250565aae5f7
Auspitz <aromir4eg@gmail.com>
2025-07-02 14:33:45 +0300

    Updated toolchain for Clang-18, removed obsolete linking

--- build.sh
+++ build.sh
@@ -64,11 +64,11 @@ if [ "$(uname)" == "Darwin" ]; then
     export CXX="$(brew --prefix)/opt/llvm/bin/clang++"
 else
     if $gcc; then
-        export CC="gcc-12"
-        export CXX="g++-12"
+        export CC="gcc-13"
+        export CXX="g++-13"
     else
-        export CC="clang-12"
-        export CXX="clang++-12"
+        export CC="clang-18"
+        export CXX="clang++-18"
     fi
 fi
 
